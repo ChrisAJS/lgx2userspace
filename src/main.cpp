53689b42120d7c39f5e9a628ec6866e362aec774
Chucky <chris@sawcz.uk>
2021-10-12 01:22:22 +0100

    Try OS X support Seems to work just as well as linux, just need to figure out virtual webcam apis

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 #include "SdlFrameOutput.h"
 
 #include "version.h"
-#include "V4LFrameOutput.h"
-#include "PulseAudioOutput.h"
 
 int main(int argc, char **argv) {
     std::cout << "lgx2userspace v0.0.0 ("<< GIT_BRANCH << "-" << GIT_REV << ")" << std::endl;
@@ -17,9 +15,7 @@ int main(int argc, char **argv) {
     lgx2::Stream *stream = new libusb::UsbStream();
 
     if (argc > 1) {
-        std::cout << "Rendering to V4L2Loopback device " << argv[1] << std::endl;
-        videoOutput = new v4l::V4LFrameOutput(argv[1]);
-        audioOutput = new pulse::PulseAudioOutput();
+        
     } else {
         std::cout << "Rendering to SDL screen" << std::endl;
         videoOutput = &sdlOutput;
