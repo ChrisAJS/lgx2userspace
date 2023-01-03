d72ab0e5dd35acb27e190b96f0385fb5679bebbe
Chris Sawczuk <chris@sawcz.uk>
2023-01-03 23:08:45 +0000

    Add GLFW based implementation to avoid using SDL

--- dependencies.cmake
+++ dependencies.cmake
@@ -10,9 +10,9 @@ IF (CMAKE_SYSTEM MATCHES Windows)
     set(ADDITIONAL_LIBS -static-libgcc -mwindows mingw32)
 endif ()
 
-if(NOT EXISTS "${CMAKE_BINARY_DIR}/conanWW.cmake")
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake" "${CMAKE_BINARY_DIR}/conanWW.cmake" TLS_VERIFY ON)
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake" TLS_VERIFY ON)
 endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
