a2833a0d728e2c876e30b34a7d29325a9e1fce93
Chris Sawczuk <chris@sawcz.uk>
2023-01-03 15:49:24 +0000

    Clean up dependencies.cmake

--- src/liblgx/dependencies.cmake
+++ src/liblgx/dependencies.cmake
@@ -1,3 +1,12 @@
+if (UNIX)
+    set(CONAN_OS Linux)
+endif ()
+
+IF (CMAKE_SYSTEM MATCHES Windows)
+    set(CONAN_OS Windows)
+    set(ADDITIONAL_LIBS -static-libgcc -mwindows mingw32)
+endif ()
+
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
 list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
 
@@ -12,24 +21,10 @@ conan_cmake_configure(REQUIRES libusb/1.0.26 sdl/2.26.1 GENERATORS cmake_find_pa
 
 conan_cmake_autodetect(settings)
 
-if (UNIX)
-    conan_cmake_install(PATH_OR_REFERENCE . BUILD missing REMOTE conancenter SETTINGS ${settings})
+conan_cmake_install(PATH_OR_REFERENCE . BUILD missing REMOTE conancenter SETTINGS ${settings} os=${CONAN_OS})
 
-    find_package(libusb)
-    find_package(SDL2)
+find_package(libusb)
+find_package(SDL2)
 
-    set(PLATFORM_LIBS SDL2::SDL2main SDL2::SDL2 libusb::libusb)
-    set(PLATFORM_INCLUDES ${SDL2_INCLUDE_DIRS} ${libusb_INCLUDE_DIRS})
-endif ()
-
-IF (CMAKE_SYSTEM MATCHES Windows)
-
-    conan_cmake_install(PATH_OR_REFERENCE . BUILD missing REMOTE conancenter SETTINGS ${settings} os=Windows)
-
-    find_package(libusb)
-    find_package(SDL2)
-
-    set(PLATFORM_LIBS mingw32 SDL2::SDL2main SDL2::SDL2 libusb::libusb -static-libgcc -mwindows)
-    set(PLATFORM_INCLUDES ${SDL2_INCLUDE_DIRS} ${libusb_INCLUDE_DIRS})
-
-endif ()
+set(PLATFORM_LIBS SDL2::SDL2main SDL2::SDL2 libusb::libusb ${ADDITIONAL_LIBS})
+set(PLATFORM_INCLUDES ${SDL2_INCLUDE_DIRS} ${libusb_INCLUDE_DIRS})
